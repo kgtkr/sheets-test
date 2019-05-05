5371abc8a46d2d7f5762ffa3715be7c6d288a1ac
kgtkr <tkr@kgtkrnoMacBook-Pro.local>
2019-05-05 13:33:31 +0900

    format

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import json
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('auth.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'auth.json', scope)
 gc = gspread.authorize(credentials)
 for sheet in gc.list_spreadsheet_files():
     wks = gc.open(sheet['name']).sheet1
 
-    data=wks.get_all_values()
+    data = wks.get_all_values()
 
-    s=json.dumps(data)
+    s = json.dumps(data)
 
     with open(f'data/{sheet["name"]}.json', mode='w') as f:
         f.write(s)
